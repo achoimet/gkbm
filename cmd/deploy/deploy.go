bc52b0178eff9491db71364e3347245738d314de
achoimet <achoimet@gmail.com>
2020-05-09 16:19:28 +0200

    add in and out cluster auth

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -15,20 +15,13 @@ limitations under the License.
 */
 
 // Note: the example only works with the code within the same release/branch.
-package deploy
+package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
-
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/achoimet/gkbm/internal/kubernetes"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -41,56 +34,15 @@ import (
 )
 
 func main() {
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	clientSet, err := kubernetes.AuthenticateOutOfCluster()
 	if err != nil {
 		panic(err.Error())
 	}
-
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
+	namespaces, err := clientSet.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	for namespace := range namespaces.Items {
+		fmt.Print(namespace)
 	}
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
 }
--- internal/kubernetes/authentication.go
+++ internal/kubernetes/authentication.go
@@ -1,14 +1,19 @@
 package kubernetes
 
 import (
+	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
+	"os"
+	"path/filepath"
 	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 )
 
-func AuthenticateToEks(clusterName string, cluster *api.Cluster, roleArn string, session *session.Session) (clientcmd.ClientConfig, error) {
+func AuthenticateToEks(clusterName string, cluster *api.Cluster, roleArn string, session *session.Session) (*kubernetes.Clientset, error) {
 
 	clusters := make(map[string]*api.Cluster)
 	clusters[clusterName] = cluster
@@ -24,6 +29,57 @@ func AuthenticateToEks(clusterName string, cluster *api.Cluster, roleArn string,
 		return nil, fmt.Errorf("failed to get token for eks: %v", err)
 	}
 	clientConfig := clientcmd.NewDefaultClientConfig(*c, &clientcmd.ConfigOverrides{Context: api.Context{Cluster: clusterName}, AuthInfo: api.AuthInfo{Token: t.Token}})
+	config, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client config: %v", err)
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate client set: %v", err)
+	}
+	return clientSet, nil
+}
 
-	return clientConfig, nil
+func AuthenticateInCluster() (*kubernetes.Clientset, error) {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client config: %v", err)
+	}
+	// creates the clientset
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate client set: %v", err)
+	}
+	return clientSet, nil
+}
+
+func AuthenticateOutOfCluster() (*kubernetes.Clientset, error) {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client config: %v", err)
+	}
+
+	// create the clientset
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate client set: %v", err)
+	}
+	return clientSet, nil
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
